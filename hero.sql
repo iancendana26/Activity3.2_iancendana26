0484c209a9924d78e200b5b4f5b0a90eebd4f6e6
Ian Kristofer Cendana <iancendana26@gmail.com>
2023-11-08 23:40:17 +0800

    feat/select-avg-playerlevel-per-class

--- hero.sql
+++ hero.sql
@@ -173,3 +173,10 @@ VALUES
         FROM Class
         WHERE class_description LIKE '&Archer%'
     );
+
+SELECT Class.class_description, AVG(Player.player_level) AS avg_level
+    FROM Player
+    JOIN Hero ON Player.player_id = Hero.player_id
+    JOIN Class ON Hero.class_id = Class.class_id
+    GROUP BY Class.class_description
+    ORDER BY avg_level DESC;
